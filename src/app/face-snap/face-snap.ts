1d0a82e7cea503f1f721076d62e238591f517790
Saikou BAH <infosaikoubah@gmail.com>
2025-11-24 22:37:22 +0100

    Utilisation du signal sur le composant enfant face-snap

--- src/app/face-snap/face-snap.ts
+++ src/app/face-snap/face-snap.ts
@@ -1,17 +1,19 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, input } from '@angular/core';
 import { FaceSnapModel } from '../models/face-snap';
 
 @Component({
   selector: 'app-face-snap',
   standalone:true,
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './face-snap.html',
   styleUrl: './face-snap.scss',
 })
 export class FaceSnap implements OnInit {
   
-  @Input() faceSnap!: FaceSnapModel;
+  //@Input() faceSnap!: FaceSnapModel;
+
+  faceSnap = input<FaceSnapModel>();
 
   snappButton = 'Oh snap'
   userSnapped = true;
@@ -28,13 +30,13 @@ export class FaceSnap implements OnInit {
   }
   
   unSnap() {
-    this.faceSnap.addSnap();
+    this.faceSnap()?.addSnap();
     this.snappButton = "Oh snap";
     this.userSnapped = false;
   }
 
   snap(): void {
-    this.faceSnap.removeSnaps();
+    this.faceSnap()?.removeSnaps();
     this.snappButton = "Oooh unSnap";
     this.userSnapped = true;
   }
